b40ebcd1b1fd36865103a7bc1872428b1c66e412
rparizi <parizirb@gmail.com>
2021-01-20 17:10:07 -0300

    ajuste geraResponse

---
+++ calculaOperacoes.py
@@ -0,0 +1,5 @@
+#realiza o calculo da soma
+def calculaSoma(num1, num2):
+    soma = num1 + num2
+    return {"soma": soma}
+
--- routes.py
+++ routes.py
@@ -1,12 +1,28 @@
 import os
 from flask import Flask, request
-
+from calculaOperacoes import calculaSoma
 
 app = Flask("Helius")
 
 @app.route("/olamundo",methods=["GET"])
 def olaMundo():
-    return geraResponse(200,"Ola mundo")
+    return geraResponse(200, "Ola mundo")
+
+#calculo de soma
+@app.route("/calcula/soma",methods=["POST"])
+def soma():
+    body = request.get_json()
+    print(body)
+
+    #analisa se os campos foram preenchidos
+    if("num1" not in body):
+        return geraResponse(400,"O parâmetro num1 é obrigatório")
+    if ("num2" not in body):
+        return geraResponse(400, "O parâmetro num1 é obrigatório")
+
+    #chama função soma
+    soma = calculaSoma(body["num1"],body["num2"])
+    return geraResponse(200,"Soma realizada com sucesso","soma",soma)
 
 
 #funcao para gerenciar responses
@@ -19,6 +35,7 @@ def geraResponse(status,mensagem, nome_do_conteudo=False, conteudo=False):
         response[nome_do_conteudo] = conteudo
 
     return response
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
