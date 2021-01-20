346a4b6eebab1567950a1ecb936b779bb7b4f669
rparizi <parizirb@gmail.com>
2021-01-20 16:18:17 -0300

    ajustes portas e environment

--- routes.py
+++ routes.py
@@ -1,11 +1,13 @@
+import os
 from flask import Flask, request
 
 
-app = Flask("Youtube")
+app = Flask("Helius")
 
 @app.route("/olamundo",methods=["GET"])
 def olaMundo():
     return {"ola" : "mundo"}
 
-
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port)
