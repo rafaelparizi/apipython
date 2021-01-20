006196794a0f31b2877898f8f02a94a2bf39a275
rparizi <parizirb@gmail.com>
2021-01-20 16:54:32 -0300

    ajuste Procfile

--- routes.py
+++ routes.py
@@ -8,6 +8,18 @@ app = Flask("Helius")
 def olaMundo():
     return {"ola" : "mundo"}
 
+
+#funcao para gerenciar responses
+def geraResponse(status,mensagem, nome_do_conteudo=False, conteudo=False):
+    response = {}
+    response["status"] = status
+    response["mensagem"] = mensagem
+
+    if(nome_do_conteudo and conteudo):
+        response[nome_do_conteudo] = conteudo
+
+    return response
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
     app.run(host='0.0.0.0',port=port)
